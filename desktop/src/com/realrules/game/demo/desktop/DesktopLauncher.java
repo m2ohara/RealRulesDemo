8e7edc51a2de4cc2deee70e22021a20426024c76
michael O' hara <m2.ohara@gmail.com>
2015-02-19 14:36:41 +0000

    Libgdx project configuration

---
+++ desktop/src/com/realrules/game/demo/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.realrules.game.demo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.realrules.game.demo.DemoGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DemoGame(), config);
+	}
+}
