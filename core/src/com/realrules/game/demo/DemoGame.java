ef76b5b7bb96d857491bbfbccba0ed5a76735c97
michael O' hara <m2.ohara@gmail.com>
2015-02-24 16:53:36 +0000

    Implemented screen updates, button listeners, actor logic

--- core/src/com/realrules/game/demo/DemoGame.java
+++ core/src/com/realrules/game/demo/DemoGame.java
@@ -1,31 +1,59 @@
 package com.realrules.game.demo;
 
+import java.util.List;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Action;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Array;
 
 public class DemoGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture background;
+	public Stage stage;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("screens//TitleScreen.png");
+		stage = new Stage();
+		Gdx.input.setInputProcessor(stage);
+		setTitleScreen();
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		stage.draw();
 	}
 	
 	private void setTitleScreen() {
+		
+		setToStage(getImage("TitleScreen", "screens//screensPack"), 0, 0);
+		Actor btn = getButton("PlayGameBtn");
+		setToStage(btn, 0, -260);
+		
+		btn.addListener(new ClickListener() {
+			 public void clicked(InputEvent event, float x, float y) {
+				 setSpeechScreen();
+			 }
+		});
 		
 	}
 	
@@ -33,11 +61,155 @@ public class DemoGame extends ApplicationAdapter {
 		
 	}
 	
-	private void setIdeaCreationScreen() {
+	private void setSpeechScreen() {
 		
-		img = new Texture("screens//TitleScreen.png");
+		setToStage(getImage("SpeechScreen", "screens//screensPack"), 0, 0);
+		Actor btn = getButton("CreateSpeechBtn");
+		setToStage(btn, 0, -260);
 		
+		btn.addListener(new ClickListener() {
+			 public void clicked(InputEvent event, float x, float y) {
+				 displaySpeechScroll();
+			 }
+		});
 	}
 	
+	private void displaySpeechScroll() {
+		Actor icon = getImage("Scroll", "icons//iconsPack");
+		setToStage(icon, 0, -60);
+		
+		Actor btn = getButton("PlayGameBtn");
+		setToStage(btn, 0, -260);
+		
+		btn.addListener(new ClickListener() {
+			 public void clicked(InputEvent event, float x, float y) {
+				 setDebateScreen();
+			 }
+		});
+	}
 	
+	private void setDebateScreen() {
+		setToStage(getImage("DebateScreen", "screens//screensPack"), 0, 0);
+		
+		Actor player = getImage("PlayerSprite", "sprites//");
+		setToStage(player, -50, 0);
+		
+		player.addAction(new Action() {
+
+			@Override
+			public boolean act(float delta) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+		
+		});
+	}
+	
+	private Actor getButton(String type) {
+		TextureAtlas buttonAtlas;
+		Skin skin = new Skin();
+		BitmapFont font = new BitmapFont();
+		TextButtonStyle style = new TextButtonStyle();
+		
+		buttonAtlas = new TextureAtlas(Gdx.files.internal("buttons//buttonsPack.pack"));
+		skin.addRegions(buttonAtlas);
+		style.font = font;
+		style.up = skin.getDrawable(type);
+		style.down = skin.getDrawable(type);
+		
+		return new TextButton("", style);
+	}
+	
+	private Actor getImage(String type, String pack) {
+
+		TextureAtlas txAtlas;
+		Skin txSkin;
+		
+		txAtlas = new TextureAtlas(Gdx.files.internal(pack+".pack"));
+		txSkin = new Skin(txAtlas);
+		
+		return new Image(txSkin.getDrawable(type));
+	}
+	
+	private TextureRegion getTextureRegion(String type) {
+		
+		TextureAtlas txAtlas;
+		
+		txAtlas = new TextureAtlas(Gdx.files.internal("sprites//"+type+".png"));
+		TextureRegion txRegion = txAtlas.findRegion(type);
+		
+		return txRegion;
+	}
+	
+	public void setToStage(Actor actor, float _xCentreOffset, float _yCentreOffset) {
+		
+		setRelativePosition(actor, _xCentreOffset, _yCentreOffset);
+		//Add actor to stage for physics processing
+		stage.addActor(actor);
+	}
+	
+	protected void setRelativePosition(Actor actorToSet, float _xCentreOffset, float _yCentreOffset) {
+		
+		//Centre actor
+		float x = (Gdx.graphics.getWidth() - actorToSet.getWidth()) /2 ;
+		float y = (Gdx.graphics.getHeight()  - actorToSet.getHeight()) / 2;
+		
+		x += _xCentreOffset;
+		y += _yCentreOffset;
+		
+		actorToSet.setPosition(x, y);
+	}
+	
+	public class Character extends Image {
+		
+	    TextureRegion[] argueFrames;
+	    private String type;
+	    private TextureRegion currentFrame;
+	    
+	    public Animation argue;
+	    public Actor actor;
+		
+		public Character(String type) {
+			this.type = type;
+			
+			createAnimation();
+			
+		}
+		
+		private Character() {
+			super(argueFrames[0]);
+		}
+		
+		private TextureRegion getTextureRegion(String type) {
+			TextureAtlas txAtlas;
+			
+			txAtlas = new TextureAtlas(Gdx.files.internal("sprites//"+type+".png"));
+			
+			TextureRegion txRegion = txAtlas.findRegion(type);
+			
+			return txRegion;
+		}
+		
+		private void createAnimation() {
+			TextureAtlas txAtlas;
+			
+			txAtlas = new TextureAtlas(Gdx.files.internal("sprites//"+type+".png"));
+			Array<AtlasRegion> regions  = (Array<AtlasRegion>) txAtlas.getRegions();
+
+			argue = new Animation(0.25f, regions);
+		
+		}
+		
+		private void setCurrentFrame() {
+			currentFrame =  argue.getKeyFrame(Gdx.graphics.getDeltaTime(), true);
+		}
+		
+		   @Override
+		public void act(float delta )
+		{
+		        super.act( delta );
+		        setCurrentFrame();
+		}
+	}
+
 }
