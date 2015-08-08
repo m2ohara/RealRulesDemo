54e734e2c6364caabcb7288e3e276298d08d90d2
m2ohara <m2.ohara@gmail.com>
2015-08-08 17:32:17 +0100

    Implemented connector/influence logic

--- core/src/com/realrules/game/interact/InfluencerInteractBehaviour.java
+++ core/src/com/realrules/game/interact/InfluencerInteractBehaviour.java
@@ -2,7 +2,13 @@ package com.realrules.game.interact;
 
 import java.util.Random;
 
-import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.realrules.game.demo.CoordinateSystem;
+import com.realrules.game.demo.CoordinateSystem.Coordinates;
+import com.realrules.game.demo.GameProperties;
 import com.realrules.game.demo.HeadSprite;
 import com.realrules.game.demo.IInteraction;
 
@@ -17,11 +23,43 @@ public class InfluencerInteractBehaviour implements IInteraction {
 		if(interactee.status == 0) { // && interactee.isActive == true) {
 			if(rand.nextFloat() > interactor.argueSuccessP) {
 				interactee.status = 2;
-				interactee.setColor(Color.CYAN);
+				setConnectorSprite(interactor);
+				setInfluenceSprite(interactee);
 				System.out.println("Follower influenced");
 			}
 		}
 		
+	}
+	
+	private void setConnectorSprite(HeadSprite interactor) {
+		Coordinates direction = CoordinateSystem.getCoordDirection(interactor.getDirection(), (int)interactor.getRotation());
+		
+		Actor connector = new Image(new TextureAtlas(Gdx.files.internal("sprites//connectorPack.pack")).getRegions().get(1));
+
+		connector.setOrigin(connector.getWidth()/2, connector.getHeight()/2);
+		connector.setPosition(interactor.getStartingX() - 20, interactor.getStartingY() -22);
+		
+		if(direction == Coordinates.E) {
+			connector.rotateBy(270);
+		}
+		else if(direction == Coordinates.S) {
+			connector.rotateBy(180);
+		}
+		else if(direction == Coordinates.W) {
+			connector.rotateBy(90);
+		}
+		
+		
+		GameProperties.get().addActorToStage(connector);
+	}
+	
+	private void setInfluenceSprite(HeadSprite interactee) {
+		Actor connector = new Image(new TextureAtlas(Gdx.files.internal("sprites//influenceDisk.pack")).getRegions().get(1));
+
+		connector.setOrigin(connector.getWidth()/2, connector.getHeight()/2);
+		connector.setPosition(interactee.getStartingX(), interactee.getStartingY());
+		
+		GameProperties.get().addActorToStage(connector);
 	}
 
 }
