cf392d25c2600f1bda4062020d6caac7901bbf45
m2ohara <m2.ohara@gmail.com>
2015-11-24 21:58:17 +0000

    Implementing db logic

--- core/src/com/realrules/data/FollowerRepo.java
+++ core/src/com/realrules/data/FollowerRepo.java
@@ -5,9 +5,12 @@ import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
+import com.realrules.game.main.Game.Head;
+import com.realrules.game.state.Follower;
 
 public class FollowerRepo {
 	
@@ -32,11 +35,11 @@ public class FollowerRepo {
 		return instance;
 	}
 
-	public int insert() {
+	public int createDB() {
 		Statement createDBStatement = null;
 		
 		//Get sql from file
-		FileHandle createDBFile = Gdx.files.internal("createDB.sql");
+		FileHandle createDBFile = Gdx.files.internal("db//CreateDBScript.sql");
 		String fileSQL = createDBFile.readString();
 		
 		//Initialise as false
@@ -74,7 +77,7 @@ public class FollowerRepo {
 		return -1;
 	}
 	
-	public ResultSet get() {
+	public ResultSet getTables() {
 		ResultSet queryResult = null;
 		
 		try {
@@ -96,6 +99,53 @@ public class FollowerRepo {
 		}
 		
 		return queryResult;
+	}
+	
+	public ArrayList<Follower> getFollowers() {
+		
+		Statement getFollowersStatement = null;
+		
+		//Create SQL		
+		String SQL = "SELECT ft.ID, ft.IMAGE_PATH FROM FOLLOWER f JOIN FOLLOWERTYPE ft ON f.TYPE_ID = ft.ID ";
+		
+		//Initialise
+		ResultSet result = null;
+		
+		try {
+			
+			//Create user
+			getFollowersStatement = connection.createStatement();
+			result = getFollowersStatement.executeQuery(SQL);
+		}
+		catch(SQLException ex) {
+			//TO DO: Log error
+			System.out.println("Error inserting user: "+ex);
+		}
+		finally {
+			try {
+				getFollowersStatement.close();
+				connection.close();
+			}
+			catch(SQLException e) {
+				//TO DO: Log error
+				System.out.println("Error closing statement: "+e);
+			}
+		}
+		
+		ArrayList<Follower> followers = new ArrayList<Follower>();
+		
+		try {
+			while(result.next()) {
+				int id = result.getInt(0);
+				String imagePath = result.getString(1);
+				followers.add(new Follower(Head.GOSSIPER, id, imagePath));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return followers;
 	}
 
 }
--- core/src/com/realrules/game/state/PlayerState.java
+++ core/src/com/realrules/game/state/PlayerState.java
@@ -26,7 +26,8 @@ public class PlayerState {
 	
 	private PlayerState() {
 		
-		FollowerRepo.getInstance();
+//		FollowerRepo.getInstance().createDB();
+		FollowerRepo.getInstance().getFollowers();
 		
 	}
 	
